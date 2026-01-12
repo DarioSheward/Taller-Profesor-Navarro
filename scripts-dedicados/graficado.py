a05f01f1f4aa65758db2131c81644b5e43961819
Dario Sheward <dsheward05@gmail.com>
2026-01-12 18:53:24 -0300

    Commit limpio :<

--- scripts-dedicados/graficado.py
+++ scripts-dedicados/graficado.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 with h5py.File('./scripts-dedicados/data.h5', 'r') as f:
     # Acceder a variables específicas
     t=f['tiempo'][:]
+    print(f['p'].shape)
 
-    for i in range(len(t)):
-        theta = f['theta'][i,:]
-        p = f['p'][i,:]
-        print(theta.shape, p.shape)
+    for i in range(t.shape[0]): # Queda buscar como poner el ... resuelto
+        theta = f['theta'][:,i]
+        p = f['p'][:,i]
     plt.hist2d(theta, p, bins=120, range=[[-np.pi,np.pi], [-2, 2]], cmap='plasma', cmin=1)  #Usemos cmin=1 para evitar problemas con bins vacíos    
     plt.xlabel('Theta') 
     plt.ylabel('p') 
--- scripts-dedicados/simulacion.py
+++ scripts-dedicados/simulacion.py
@@ -20,7 +20,8 @@ p=p.flatten()
 
 t=100   # numero de pasos temporales
 dt=0.01   # paso temporal propuesto
-tem=np.arange(0,t,dt)     #array de tiempos
+tem=np.arange(0,t,dt)
+
 
 def M_y(theta):
     M_y=np.mean(np.sin(theta))
@@ -30,39 +31,42 @@ def M_x(theta):
     M_x=np.mean(np.cos(theta))
     return M_x
 
-def A(theta):           
+def A(theta):
     return M_y(theta)*np.cos(theta)-M_x(theta)*np.sin(theta)
 # creamos el archivo con los arrays
 with h5py.File('scripts-dedicados/data.h5', 'w') as f:
     # Creamos el dataset inicial (0 filas, N columnas)
     # maxshape=(None, 10) permite filas infinitas
-    dset_theta = f.create_dataset('theta', shape=(0, N), maxshape=(None, N), dtype='float32')
+    dset_theta = f.create_dataset('theta', shape=(N, 0), maxshape=(N, None), dtype='float64')
+    dset_theta[:,] = theta  # Guardamos el estado inicial
     
-    dset_tiempo= f.create_dataset('tiempo', data=tem, dtype='float32')
+    dset_tiempo= f.create_dataset('tiempo', data=tem, dtype='float64')  #aun por implementar
+    dset_tiempo=tem
+    dset_p = f.create_dataset('p', shape=(N, 0), maxshape=(N, None), dtype='float64')
+    dset_p[:,] = p  # Guardamos el estado inicial
+    dset_M_y = f.create_dataset('M_y', shape=(1,), maxshape=(None,), dtype='float64')
+    dset_M_y[0,] = M_y(theta)  # Guardamos el estado inicial
+    dset_M_x = f.create_dataset('M_x', shape=(1,), maxshape=(None,), dtype='float64')
+    dset_M_x[0,] = M_x(theta)  # Guardamos el estado inicial
 
-    dset_p = f.create_dataset('p', shape=(0, N), maxshape=(None, N), dtype='float32')
-    
-    dset_M_y = f.create_dataset('M_y', shape=(0,), maxshape=(None,), dtype='float32')
-    
-
-    dset_M_x = f.create_dataset('M_x', shape=(0,), maxshape=(None,), dtype='float32')
-    
-    for i in range(t+1):
+    for i in tem:
         # Guardamos los nuevos datos al final
-        dset_theta.resize(dset_theta.shape[0]+theta.shape[0], axis=0)
-        dset_theta[-1] = theta
+        p=p + dt*A(theta)*0.5
+        theta=theta +   p*dt    #0.01=p[1]-p[0] ayudaaaa quitenme la ia, sabe siempre lo que intento hacer 
+        p=p + dt*A(theta)*0.5   
 
-        dset_p.resize(dset_p.shape[0]+p.shape[0], axis=0)
-        dset_p[-1] = p
+        dset_theta.resize(dset_theta.shape[1]+1, axis=1)
+        dset_theta[:,-1] = theta
 
-        dset_M_y.resize(dset_M_y.shape[0]+1, axis=0)
+        dset_p.resize(dset_p.shape[1]+1, axis=1)
+        dset_p[:,-1] = p
+        
+        dset_M_y.resize(dset_M_x.shape[0]+1, axis=0)
         dset_M_y[-1] = M_y(theta)
 
         dset_M_x.resize(dset_M_x.shape[0]+1, axis=0)
         dset_M_x[-1] = M_x(theta)
-        p=p + dt*A(theta)*0.5
-        theta=theta +   p*dt    #0.01=p[1]-p[0] ayudaaaa quitenme la ia, sabe siempre lo que intento hacer 
-        p=p + dt*A(theta)*0.5   
+
     f.close()
  #XCreo que no necesitamos guardar los valores de t sino que informar la cantidad de pasos dados... es mejor dejar un dataset dado ndjsndjs.
 
